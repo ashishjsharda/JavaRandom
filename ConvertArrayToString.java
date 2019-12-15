0fd6476bcf634013e383965f8b9441fd773fe4bc
Ashish S <ashishjsharda@gmail.com>
2019-12-15 11:44:46 -0500

    Create ConvertArrayToString.java

---
+++ ConvertArrayToString.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+/**
+ * Convert Array to String
+ * @author asharda
+ *
+ */
+public class ConvertArrayToString {
+
+	public static void main(String cp[])
+	{
+		char arr[]= {'a','b','c'};
+		System.out.println(Arrays.toString(arr));
+
+	}
+
+}
