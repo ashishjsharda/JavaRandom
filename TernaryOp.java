507e9c5eea0d3429b5b3a0e07f97dd37f3e917b9
Ashish S <ashishjsharda@gmail.com>
2020-01-11 08:08:55 -0500

    Create TernaryOp.java

---
+++ TernaryOp.java
@@ -0,0 +1,17 @@
+package test;
+
+
+/**Using Ternary Operator Java
+ * @author ashish
+ *
+ */
+public class TernaryOp {
+
+	public static void main(String[] args) {
+
+		int num=20;
+		String msg=num>10?"Yes Number is greater than 10":"Number is less than 10";
+		System.out.println(msg);
+	}
+
+}
