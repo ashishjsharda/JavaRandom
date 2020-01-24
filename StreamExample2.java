ad224469df6d3ac99c6763a9a9ca3e2110510e6c
Ashish S <ashishjsharda@gmail.com>
2020-01-23 22:36:06 -0500

    Create  StreamExample2.java

---
+++ StreamExample2.java
@@ -0,0 +1,11 @@
+import java.util.stream.IntStream;
+/**
+ * Using Streams
+ * @author ashish
+ */
+public class StreamExample2 {
+    public static void main(String[] args) {
+
+        System.out.println(IntStream.range(1,5).sum());
+    }
+}
