5646d97cc6dee5669120345406fb1e7258b6b6dc
Ashish S <ashishjsharda@gmail.com>
2020-01-27 08:01:54 -0500

    Create EpochTime.java

---
+++ EpochTime.java
@@ -0,0 +1,16 @@
+import java.time.Instant;
+
+/**
+ * Find Epoch Time
+ * @author ashish
+ *
+ */
+public class EpochTime {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Time seen is " +Instant.now().toEpochMilli());
+
+	}
+
+}
