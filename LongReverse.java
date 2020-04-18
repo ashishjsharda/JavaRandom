db0417b15ea9920363df908c521447ce0a87daa4
Ashish S <ashishjsharda@gmail.com>
2020-04-17 21:48:45 -0400

    Create LongReverse.java

---
+++ LongReverse.java
@@ -0,0 +1,13 @@
+
+/**
+ * Using Long Reverse Function
+ * @author ashish
+ */
+public class LongReverse {
+
+    public static void main(String[] args) {
+        long num=8;
+        System.out.println(Long.reverse(num));
+    }
+
+}
