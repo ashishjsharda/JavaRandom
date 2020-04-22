883b38abf4988a077dbbd7a80f6b93737fc34b85
Ashish S <ashishjsharda@gmail.com>
2020-04-22 14:25:47 -0400

    Create Stream6.java

---
+++ Stream6.java
@@ -0,0 +1,10 @@
+import java.util.stream.IntStream;
+/**
+ * StreamRange
+ * @author ashish
+ */
+public class Stream6 {
+    public static void main(String[] args) {
+        IntStream.range(1,10).forEach(System.out::println);
+    }
+}
