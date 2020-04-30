c68b6a0327b7ba22823800f1d9d8fa54a7724b65
Ashish S <ashishjsharda@gmail.com>
2020-04-30 06:17:31 -0400

    Create Stream14.java

---
+++ Stream14.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Using Streams Part 14
+ * @author ashish
+ */
+public class Stream14 {
+    public static void main(String[] args) throws IOException {
+        Stream<String> rows= Files.lines(Paths.get("data.txt"));
+        Map<String,Integer> map=new HashMap<String,Integer>();
+        map=rows.map(x->x.split(",")).filter(x->x.length==3).filter(x->Integer.parseInt(x[1])>15).collect(Collectors.toMap(x->x[0],x->Integer.parseInt(x[1])));
+        for(String key:map.keySet()){
+            System.out.println(key + " "+map.get(key));
+        }
+    }
+}
