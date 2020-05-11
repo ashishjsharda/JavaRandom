434bf3c560a9455eca74e86b4b0cb5790c3ae7d8
Ashish S <ashishjsharda@gmail.com>
2020-05-11 17:12:07 -0400

    Create Function2.java

---
+++ Function2.java
@@ -0,0 +1,12 @@
+package Java8;
+import java.util.function.Function;
+
+/**
+ * Using Function
+ * @author ashish
+ */
+public class Function2 {
+    public static void main(String[] args) {
+        System.out.println(Function.identity());
+    }
+}
