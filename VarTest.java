02d12ccc79af40047f869f2c68e2e7c1f9bac5c6
Ashish S <ashishjsharda@gmail.com>
2020-08-05 19:59:21 -0400

    Create VarTest.java

---
+++ VarTest.java
@@ -0,0 +1,13 @@
+/**
+ * Using Vars in Java
+ * @author ashish
+ */
+public class VarTest {
+    public static void main(String[] args) {
+        var a=10;
+        System.out.println(a);
+        var c="Hello";
+        System.out.println(c);
+
+    }
+}
