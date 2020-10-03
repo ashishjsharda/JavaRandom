f9d3b2a13b46ffb2a33a2936d636c1e4b2b0dd17
Ashish S <ashishjsharda@gmail.com>
2020-10-03 17:18:22 -0400

    Create MultipleLines.java

---
+++ MultipleLines.java
@@ -0,0 +1,10 @@
+
+/**
+ * Print on multiple Lines
+ * @author ashish
+ */
+public class MultipleLines {
+    public static void main(String[] args) {
+        System.out.printf("%s%n%s%n","Welcome To","Java Programming");
+    }
+}
