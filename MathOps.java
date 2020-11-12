6cd070eeaf5081d32d913fd2b0663208cfa6ae49
Ashish S <ashishjsharda@gmail.com>
2020-11-12 16:53:15 -0500

    Create MathOps.java

---
+++ MathOps.java
@@ -0,0 +1,16 @@
+/**
+ * Example using Math Ops
+ * @author ashish
+ */
+
+public class MathOps {
+    public static void main(String[] args) {
+        System.out.println(Math.max(10,20));
+        System.out.println(Math.min(10,20));
+        System.out.println(Math.abs(10));
+        System.out.println(Math.pow(10,2));
+        System.out.println(Math.exp(10));
+        System.out.println(Math.sin(10));
+        System.out.println(Math.cos(10));
+    }
+}
