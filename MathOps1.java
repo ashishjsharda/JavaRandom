c2da58b15bef3755e24fce735b1e6f9fc32e98ba
Ashish S <ashishjsharda@gmail.com>
2021-02-26 06:38:20 -0500

    Create MathOps1.java

---
+++ MathOps1.java
@@ -0,0 +1,16 @@
+/**
+ *Using Math Ops
+ * @author ashish
+ */
+public class MathOps {
+    public static void main(String[] args) {
+        int max=Math.max(5,10);
+        double d=1.0/0.0;
+        if(d==Double.POSITIVE_INFINITY)
+        {
+            System.out.println("Positive Infinity");
+
+        }
+        System.out.println(max);
+    }
+}
