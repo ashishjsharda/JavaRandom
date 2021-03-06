3c9d74fadac938df43155a7947f3528bd3e2a2af
Ashish S <ashishjsharda@gmail.com>
2021-03-06 13:42:08 -0500

    Create InstantEx.java

---
+++ InstantEx.java
@@ -0,0 +1,12 @@
+import java.time.Instant;
+
+/**
+ *Example using Instant
+ * @author ashish
+ */
+public class InstantEx {
+    public static void main(String[] args) {
+        System.out.println(Instant.now());
+
+    }
+}
