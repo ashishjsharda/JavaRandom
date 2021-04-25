95a27901ef0c3f393e81ae054331b82dc0f055af
Ashish S <ashishjsharda@gmail.com>
2021-04-25 08:56:33 -0400

    Create HashCode.java

---
+++ HashCode.java
@@ -0,0 +1,13 @@
+
+/**
+ * Hashcode
+ * @author ashish
+ */
+public class HashCode {
+    public static void main(String[] args) {
+
+        String str="Implement Hash code";
+        System.out.println(str.hashCode());
+
+    }
+}
