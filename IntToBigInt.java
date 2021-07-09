5c644e0916eb667bf75e3697933ba2a2531cf904
Ashish S <ashishjsharda@gmail.com>
2021-07-08 21:12:18 -0400

    Create IntToBigInt.java

---
+++ IntToBigInt.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+
+/**
+ * Converts an int to BigInteger
+ */
+public class IntToBigInt {
+
+    public static void main(String[] args) {
+
+        int i=10;
+        BigInteger bigInteger=BigInteger.valueOf(i);
+        System.out.println(bigInteger);
+    }
+}
